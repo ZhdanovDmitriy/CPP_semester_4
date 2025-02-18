916fb1c01ab2896459c0a5bce97c029853b2ef19
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-02-18 16:35:12 +0300

    Реализована функция решения квадратного уравнения, исправлены коллизии в функции проверки корректности коэффициентов, добавлена функция выведения заголовка

--- lab1/functions.h
+++ lab1/functions.h
@@ -7,5 +7,7 @@
 #include <regex>
 #include <cmath>
 
-std::vector<long double> getCoefficients(std::string str);
-std::pair<long double, long double> solveQuadraticEquation(std::vector<long double> coefficients);
+std::vector<long double> getCoefficients(std::string& const str);
+bool isCorrectQuadraticEquation(std::vector<long double>& const coefficients);
+std::pair<long double, long double> solveQuadraticEquation(std::vector<long double>& const coefficients);
+void printHead();
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,9 +2,12 @@
 
 int main() {
     std::string str;
+    printHead();
     std::getline(std::cin, str);
     std::vector<long double> coefficients = getCoefficients(str);
-    std::pair<long double, long double> ans = solveQuadraticEquation(coefficients);
-    std::cout << "Answer: " << ans.first << " ; " << ans.second << std::endl;
+    if (isCorrectQuadraticEquation(coefficients)) {
+        std::pair<long double, long double> ans = solveQuadraticEquation(coefficients);
+        std::cout << "Answers: x = " << ans.first << " and x = " << ans.second << std::endl;
+    }
     return 0;
 }
